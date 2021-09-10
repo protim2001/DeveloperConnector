63978c63765bddebf8fe7e537b24dc83340f8cbf
Protim Aich <protimaich2001@gmail.com>
2021-09-11 03:00:47 +0530

    Second commit

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require('express')
+const express = require('express');
+const connectDB =require('./config/db');
 
-const app = express();
+const app = express(); 
+//Connect database
+connectDB();
 
 app.get('/' , (req,res) => res.send('API Running'));
+
+//define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 

b3d3cb7ad03403aea7e92613b5c7cc4f19d9672a
Protim Aich <protimaich2001@gmail.com>
2021-11-24 02:30:32 +0530

    Ninth commit

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const auth = require('../../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const Profile = require('../../models/Profile');
 const User = require('../../models/User');
+const Post = require('../../models/Post');
 
 // @route    GET api/profile/me
 // @desc     Get current users profile
@@ -36,7 +37,7 @@ router.post('/',[
     check('status', 'Status is required')
     .not()
     .isEmpty(),
-    check('status', 'Status is required')
+    check('skills', 'Skills is required')
     .not()
     .isEmpty(),
   ]
@@ -136,7 +137,8 @@ router.get('/user/:user_id',async(req,res)=>{
 // @access   Private
 router.delete('/',auth,async(req,res)=>{
   try{
-    //@todo- remove users posts
+    //remove users posts
+    await Post.deleteMany({user:req.user.id});
 
     //Remove profile 
       await Profile.findOneAndRemove({ user: req.user.id });

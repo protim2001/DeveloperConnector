570148bdc40cc6ac6f739a5ea60128428d334d53
Protim Aich <protimaich2001@gmail.com>
2021-12-03 03:23:21 +0530

    Eleventh commit

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,8 @@ import AddExperience from './components/profile_form/AddExperience';
 import AddEducation from './components/profile_form/AddEducation';
 import Profiles from './components/profiles/Profiles';
 import Profile from './components/profile/Profile';
+import Posts from './components/posts/Posts';
+import Post from './components/post/Post';
 import PrivateRoute from './components/routing/PrivateRoute';
 //Redux
 import { Provider } from 'react-redux';
@@ -45,7 +47,9 @@ const App = () => {
                     <PrivateRoute exact path="/create-profile" component={CreateProfile} />
                     <PrivateRoute exact path="/edit-profile" component={EditProfile} />
                     <PrivateRoute exact path="/add-experience" component={AddExperience} />
-                    <PrivateRoute exact path="/add-education" component={AddEducation} />
+                    <PrivateRoute exact path="/add-education" component={AddEducation} /> 
+                    <PrivateRoute exact path="/posts" component={Posts} /> 
+                    <PrivateRoute exact path="/post/:id" component={Post} /> 
                 </Switch>
             </section>
     </Fragment>
--- routes/api/posts.js
+++ routes/api/posts.js
@@ -127,7 +127,6 @@ router.put('/like/:id',auth,async(req,res)=>{
 router.put('/unlike/:id',auth,async(req,res)=>{
     try{
         const post = await Post.findById(req.params.id);
-
         //Check if the post has already been liked
         if(post.likes.filter(like=>like.user.toString()===req.user.id).length===0){
             return res.status(400).json({ msg: 'Post has not yet been liked' });
